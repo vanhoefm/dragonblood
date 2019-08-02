859737386468b6824de9ed3defb73ae7875ccd5a
Mathy Vanhoef <Mathy.Vanhoef@nyu.edu>
2019-08-02 17:12:39 +0400

    Major updated of website

---
+++ gen-qa-index.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from bs4 import BeautifulSoup
+
+def gen_qa_index(filename):
+	soup = BeautifulSoup(open(filename), "html.parser")
+	faq = soup.find("a", {"name": "faq"})
+	faq_titles = faq.find_next_siblings("h3")
+	faq_anchors = faq.find_next_siblings("a")
+	assert len(faq_titles) == len(faq_anchors)
+
+	print("<ul>")
+	for title, anchor in zip(faq_titles, faq_anchors):
+		print('\t<li><a href="#{anchor}">{title}</a></li>'.format(title=title.text, anchor=anchor["name"]))
+	print("</ul>")
+
+
+if __name__ == "__main__":
+	gen_qa_index("index.html")
+
